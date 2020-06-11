2e8f7da8613ebb75216360f3544eeb3f2bf33627
taylorfsteele <taylor.f.steele@gmail.com>
2020-06-11 13:27:00 -0400

    Re-commit from breaking changes Added TrackForm Recording current track constantly flagged as opposite bool Stashed & Working from new clean dir

---
+++ track-app/src/Components/TrackForm.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Input, Button } from "react-native-elements";
+import { View, Text, StyleSheet } from "react-native";
+import { Spacing } from "../styles";
+import { Context as LocationContext } from "../context/LocationContext";
+
+const TrackForm = () => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+
+  //console.log(locations.length);
+
+  return (
+    <View style={styles.container}>
+      <Input
+        value={name}
+        onChangeText={changeName}
+        placeholder="Enter Track Name"
+        containerStyle={styles.input}
+      />
+      <Button title="Start" onPress={startRecording} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...Spacing.baseMargin,
+  },
+  input: {},
+  button: {},
+});
+
+export default TrackForm;
